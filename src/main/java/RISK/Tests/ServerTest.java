45a00e831402519ed11be43de6e4bd630cf93480
Boyam Hou <bh214@duke.edu>
2020-04-06 18:33:03 -0400

    changed txt in resources: balanced armies

--- src/main/java/RISK/Tests/ServerTest.java
+++ src/main/java/RISK/Tests/ServerTest.java
@@ -5,7 +5,7 @@ import RISK.RISKGameServer;
 public class ServerTest {
     public static void main(String args[]) {
         RISKGameServer.run(8000,
-                    "terr.txt",
-                  "player.txt",
-                   "army.txt"); }
+                    "territoriesJSON_2.txt",
+                  "playersJSON_2.txt",
+                   "armiesJSON_2.txt"); }
 }
