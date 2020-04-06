24ab0728d232270a6b960973b2db89ee3e09e8b2
bh214 <bh214@duke.edu>
2020-04-06 19:20:03 -0400

    mid-testing

--- src/test/java/RISK/testGameServer.java
+++ src/test/java/RISK/testGameServer.java
@@ -11,32 +11,18 @@ import RISK.Game.GameClientJSON;
 import RISK.Game.GameServerJSON;
 
 public class testGameServer {
-  @Test
-  public void test_ServerSetup() {
-    int port = 9000;
-    GameServerJSON server = getTestServer(port);
-
-    assert (server.getPlayerMap().size() == 3);
-    assert (server.getArmyMap().size() == 4);
-    assert (server.getTerrMap().size() == 4);
-  }
 
   @Test
   public void test_3Client1ServerInteraction() {
     try {
-      int port = 8000;
-      int playerNum = 3;
-
-      // setup local server first
-      GameServerJSON server = getTestServer(port);
-      ServerThread serverThread = new ServerThread(server);
+      // run server thread
+      ServerThread serverThread = new ServerThread();
       serverThread.start();
     
-      // clients connect to server
-      // and initialize 
-      ClientThread clientThreads[] = new ClientThread[playerNum];
+      // run client threads
+      ClientThread clientThreads[] = new ClientThread[3];
       // player orders
-      String orders[] = new String[playerNum];
+      String orders[] = new String[3];
       // player1: terr1(1)
       orders[0] = "";
       orders[0] += "A\n1\n4\n1\nN\n"; // attack terr4 from terr1 (round1)
@@ -53,31 +39,17 @@ public class testGameServer {
       orders[2] += "M\n4\n3\n1\nN\n";  // move 1 unit from terr4 to terr3 (round1)
       orders[2] += "A\n1\n2\n30\nN\n"; // attack terr2 from terr1 (round2)
 
+      String strongOrder = orders[2];
+      InputStream is = new ByteArrayInputStream(strongOrder.getBytes(StandardCharsets.UTF_8));
+      Scanner strongPlayerScanner = new Scanner(is);
+ 
+      
       // player3 should have won the game after round2
       
       for (int i = 0; i < clientThreads.length; i++) {
         GameClientJSON client;
         if (i != clientThreads.length - 1) {
-          // two weak player's orders
-          String weakOrder = orders[i];
-          InputStream is = new ByteArrayInputStream(weakOrder.getBytes(StandardCharsets.UTF_8));
-          Scanner weakPlayerScanner = new Scanner(is);
-          Displayer displayer = new DisplayerStub();
-          client = this.getTestClient(port, weakPlayerScanner, displayer);
-
-        } else {
-          // one strong player's scanner
-          String strongOrder = orders[2];
-          InputStream is = new ByteArrayInputStream(strongOrder.getBytes(StandardCharsets.UTF_8));
-          Scanner strongPlayerScanner = new Scanner(is);
-          Displayer displayer = new DisplayerStub();
-          client = this.getTestClient(port, strongPlayerScanner, displayer);
-
-          Displayer textDisplayer = new TextDisplayer();
-          client.getDisplayed(textDisplayer);
-          client.getDisplayed(textDisplayer, "testing text displayer!");
-        }
-        Displayer displayer = new DisplayerStub();
+      
         ClientThread clientThread = new ClientThread(client, displayer);
         clientThread.start();
         clientThreads[i] = clientThread;
@@ -95,18 +67,14 @@ public class testGameServer {
   }
 
   public class ServerThread extends Thread {
-    GameServerJSON server;
+    Scanner scanner;
 
-    public ServerThread(GameServerJSON server) {
-      this.server = server;
+    public ServerThread(Scanner scanner) {
+      this.scanner = scanner;
     }
 
     public void run() {
-      RISKGameServer.serverAcceptConnections(this.server);
-      while (server.getGameState() == 0) { // game still runs
-        RISKGameServer.serverAcceptOrders(server);
-        RISKGameServer.serverResolveCombats(server);
-      }
+      RISKGameServer.run();
       System.out.println("Server finishes game");
       //System.out.println("HHHH YEAH");
     }
@@ -188,9 +156,9 @@ public class testGameServer {
     // using the "run" funcion of client & server
     int port2 = 8321;
     //server:
-    String terrPath = "./src/main/resources/territoriesJSON_test3.txt";
-    String playerPath = "./src/main/resources/playersJSON_test3.txt";
-    String armyPath = "./src/main/resources/armiesJSON_test3.txt";
+    String terrPath = "./src/main/resources/terr_test_3.txt";
+    String playerPath = "./src/main/resources/player_test_3.txt";
+    String armyPath = "./src/main/resources/army_test_3.txt";
     ServerRunThread sThread = new ServerRunThread(port2,
                                                   terrPath,
                                                   playerPath,
