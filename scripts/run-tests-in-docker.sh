42288dd297fcdc76715880470155395173179462
bh214 <bh214@duke.edu>
2020-04-27 17:46:00 -0400

    Update run-tests-in-docker.sh

--- scripts/run-tests-in-docker.sh
+++ scripts/run-tests-in-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir coverage
-docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
+xvfb-run -a --server-args='-screen 0, 1024x768x24' docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
