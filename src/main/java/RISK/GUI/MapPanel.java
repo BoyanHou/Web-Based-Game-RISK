c8282392b8ef7c6b8cf928c11aa15e0942677d1a
yl657 <yl657@duke.edu>
2020-04-27 20:22:12 -0400

    addgamestate

--- src/main/java/RISK/GUI/MapPanel.java
+++ src/main/java/RISK/GUI/MapPanel.java
@@ -24,16 +24,15 @@ public class MapPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        try{
-            BufferedImage img = ImageIO.read(new File("./src/main/resources/frame4.png"));
-            Image newImage =img.getScaledInstance(700, 500, Image.SCALE_DEFAULT);
-            g.drawImage(newImage,0,0,null);   
-        }
-        catch(IOException ioe){
-        }
         for (TerritoryBlock territoryBlock: territoryBlocks) {
             territoryBlock.paint(g);
         }
+//        try{
+//            BufferedImage img = ImageIO.read(new File("./src/main/resources/frame4.png"));
+//            Image newImage =img.getScaledInstance(700, 500, Image.SCALE_DEFAULT);
+//            g.drawImage(newImage,0,0,null);
+//        } catch(IOException ioe){
+//        }
 
     }
 }
